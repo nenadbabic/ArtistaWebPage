d9ca15d9cb1da8ddace7e3d0855a16e611e9082a
nenadbabic <nenadbabic995@gmail.com>
2018-01-12 18:22:02 +0100

    Langauge Fix
    Eng -> Slo

--- phpST/view/homepage.php
+++ phpST/view/homepage.php
@@ -64,41 +64,31 @@
                 <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
             </form>
             <ul class="nav navbar-nav navbar-right">
-                <li><p class="navbar-text">Already have an account?</p></li>
+                <li><p class="navbar-text">Že imate račun?</p></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Prijava</b> <span class="caret"></span></a>
                     <ul id="login-dp" class="dropdown-menu">
                         <li>
                             <div class="row">
                                 <div class="col-md-12">
-                                    Login via
-                                    <div class="social-buttons">
-                                        <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-                                        <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-                                    </div>
                                     or
                                     <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
                                         <div class="form-group">
-                                            <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                            <label class="sr-only" for="exampleInputEmail2">E-naslov</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="E-naslov" required>
                                         </div>
                                         <div class="form-group">
-                                            <label class="sr-only" for="exampleInputPassword2">Password</label>
-                                            <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                            <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+                                            <label class="sr-only" for="exampleInputPassword2">Geslo</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Geslo" required>
+                                            <div class="help-block text-right"><a href="">Ste pozabili geslo?</a></div>
                                         </div>
                                         <div class="form-group">
-                                            <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                                        </div>
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox"> keep me logged-in
-                                            </label>
+                                            <button type="submit" class="btn btn-primary btn-block">Prijava</button>
                                         </div>
                                     </form>
                                 </div>
                                 <div class="bottom text-center">
-                                    New here ? <a href="#"><b>Join Us</b></a>
+                                    New here ? <a href="<?= BASE_URL . "registration.html" ?>"><b>Registracija</b></a>
                                 </div>
                             </div>
                         </li>
